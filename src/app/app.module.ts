cdaa98d096c47bbd1b46594dcad70ac2bd039131
Datta, Chetan <Chetan_Datta@Dell.com>
2023-12-09 23:38:48 +0530

    landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BlocksComponent } from './shared/blocks/blocks.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CompanyDetailsComponent } from './shared/company-details/company-details.component';
+import { DeckComponent } from './shared/deck/deck.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BlocksComponent,
+    FooterComponent,
+    CompanyDetailsComponent,
+    DeckComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    HeaderComponent,
+    BlocksComponent,
+    FooterComponent,
+    CompanyDetailsComponent,
+    DeckComponent
+  ]
 })
 export class AppModule { }
